242f6d37210785e86f1c74779b59d9c266ad1e5d
kelvinka38 <kahoangkhanhthuan@gmail.com>
2026-08-21 13:47:09 +0700

    feat(workforce): add application entry point

---
+++ src/main/java/org/metatron/workforce/WorkforceApplication.java
@@ -0,0 +1,12 @@
+package org.metatron.workforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkforceApplication.class, args);
+    }
+}
